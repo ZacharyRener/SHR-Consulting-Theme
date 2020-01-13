30fafb70d44bf9cd50d6baaa45aeb346f4257332
Zachary Rener <me@zacharyrener.com>
2020-01-12 23:55:10 -0600

    Home100

--- header.php
+++ header.php
@@ -67,7 +67,7 @@ defined( 'ABSPATH' ) || exit;
 					<div class="collapse navbar-collapse" id="navbarNavDropdown">
 						<ul class="navbar-nav">
 							<li class="nav-item active">
-								<a class="nav-link" href="#">Home1337 <span class="sr-only">(current)</span></a>
+								<a class="nav-link" href="#">Home100 <span class="sr-only">(current)</span></a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link" href="#">Features</a>
