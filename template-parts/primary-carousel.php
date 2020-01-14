4bf488e8faa6df3a1c0dee4723613d0fed549578
Zachary Rener <me@zacharyrener.com>
2020-01-13 21:48:49 -0600

    tidied up parallax

--- template-parts/primary-carousel.php
+++ template-parts/primary-carousel.php
@@ -6,13 +6,17 @@
   </ol>
   <div class="carousel-inner" >
 
-    <div class="carousel-item active" id="scene">
-      
-     
+    <div class="carousel-item active" >
 
-        <div class="carousel-wrapper" data-depth="0.6" style="background-image:url(http://shr.hingedev.com/wp-content/uploads/2020/01/unnamed-1.jpg);">
+        <div class="carousel-wrapper">
         
-            <div class="text-content" data-depth="0" >
+          <div id="scene">
+            <div data-depth="0.1">
+              <img src="http://shr.hingedev.com/wp-content/uploads/2020/01/unnamed-1.jpg" />
+            </div>
+          </div>
+
+          <div class="text-content" data-depth="0" >
                 <section class="slide-count">01<i class="fa fa-chevron-right"></i></section>
                 <section class="top-text"><h1>Shift to the cloud</h1></section>
                 <section class="bottom-text"><h1>Gracefully and Securely</h1></section>
