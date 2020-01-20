f136b4a8b547e9c3fca06afdbb9042478ac26d50
Zachary Rener <me@zacharyrener.com>
2020-01-20 17:04:31 -0600

    .

--- template-parts/_post_types.php
+++ template-parts/_post_types.php
@@ -129,4 +129,16 @@ function codex_news_events_init() {
 
 }
 
+if( function_exists('acf_add_options_page') ) {
+	
+	acf_add_options_page(array(
+		'page_title' 	=> 'Global Settings',
+		'menu_title'	=> 'Global Settings',
+		'menu_slug' 	=> 'global-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> false
+	));
+
+}
+
 ?>
